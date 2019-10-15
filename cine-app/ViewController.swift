021527a08ad6b14cd35cf182c618a8533ce86496
Alejandro Mendoza <zippi180@gmail.com>
2019-10-15 13:38:12 -0500

    Initial proyect set up

---
+++ cine-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cine-app
+//
+//  Created by Alejandro Mendoza on 15/10/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
