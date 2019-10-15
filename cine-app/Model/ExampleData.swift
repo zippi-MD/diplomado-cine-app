a61834344fc4d652b0f243c87b3b5239000806e8
Alejandro Mendoza <zippi180@gmail.com>
2019-10-15 13:56:23 -0500

    Added basic CinemaRoom example data

--- cine-app/Model/ExampleData.swift
+++ cine-app/Model/ExampleData.swift
@@ -13,3 +13,10 @@ let ironManMovie = Movie(name: "Iron Man", description: "Un empresario millonari
 let avengersMovie = Movie(name: "Avengers", description: "Los superhéroes se alían para vencer al poderoso Thanos, el peor enemigo al que se han enfrentado. Si Thanos logra reunir las seis gemas del infinito: poder, tiempo, alma, realidad, mente y espacio, nadie podrá detenerlo.", director: "Anthony Russo", picture: UIImage(named: "Avengers")!)
 
 let isleOfDogsMovie = Movie(name: "Isle of Dogs", description: "El alcalde de una ciudad japonesa decreta que, con motivo de una epidemia de gripe canina, todos los perros deben quedar confinados en una isla. Un niño de 12 años emprende una aventura para llegar hasta la isla y rescatar a su mascota.", director: "Wes Anderson", picture: UIImage(named: "IsleOfDogs")!)
+
+
+let room1 = CinemaRoom(movie: ironManMovie, seats: 100, roomNumber: 1, functions: ["11:00", "15:00", "20:00", "00:00"])
+
+let room2 = CinemaRoom(movie: avengersMovie, seats: 55, roomNumber: 2, functions: ["09:00", "12:00", "15:00", "18:00", "22:00", "00:00"])
+
+let room3 = CinemaRoom(movie: isleOfDogsMovie, seats: 80, roomNumber: 3, functions: ["19:00", "22:00"])
