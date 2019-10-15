d0b4cfd8342548f0015045085561a83d84f3a72f
Alejandro Mendoza <zippi180@gmail.com>
2019-10-15 13:52:58 -0500

    Added Movies examples

---
+++ cine-app/Model/ExampleData.swift
@@ -0,0 +1,15 @@
+//
+//  ExampleData.swift
+//  cine-app
+//
+//  Created by Alejandro Mendoza on 15/10/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+let ironManMovie = Movie(name: "Iron Man", description: "Un empresario millonario construye un traje blindado y lo usa para combatir el crimen y el terrorismo.", director: "Jon Favreau", picture: UIImage(named: "IronMan")!)
+
+let avengersMovie = Movie(name: "Avengers", description: "Los superhéroes se alían para vencer al poderoso Thanos, el peor enemigo al que se han enfrentado. Si Thanos logra reunir las seis gemas del infinito: poder, tiempo, alma, realidad, mente y espacio, nadie podrá detenerlo.", director: "Anthony Russo", picture: UIImage(named: "Avengers")!)
+
+let isleOfDogsMovie = Movie(name: "Isle of Dogs", description: "El alcalde de una ciudad japonesa decreta que, con motivo de una epidemia de gripe canina, todos los perros deben quedar confinados en una isla. Un niño de 12 años emprende una aventura para llegar hasta la isla y rescatar a su mascota.", director: "Wes Anderson", picture: UIImage(named: "IsleOfDogs")!)
